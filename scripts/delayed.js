3a2ef8e7e6f9f9c6e2cf3c2027ab3ba70becbe34
jaytech989 <jay.vyas@techchefz.com>
2025-05-21 13:14:30 +0530

    Update delayed.js

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,9 +1,9 @@
 // eslint-disable-next-line import/no-cycle
-// import { sampleRUM } from './aem.js';
+import { sampleRUM } from './aem.js';
 
 // Core Web Vitals RUM collection
-// sampleRUM('cwv');
-// var scriptElement = document.createElement('script');
-// scriptElement.src = 'https://assets.adobedtm.com/21b53c73144b/85a96e35869a/launch-ae0f541915c0-development.min.js';
-// scriptElement.async = true;
-// document.head.appendChild(scriptElement);
+sampleRUM('cwv');
+var scriptElement = document.createElement('script');
+scriptElement.src = 'https://assets.adobedtm.com/21b53c73144b/85a96e35869a/launch-47de65da9546-development.min.js';
+scriptElement.async = true;
+document.head.appendChild(scriptElement);
