e07cbe01836c0a7751038c4c49856b3c3ebcc0a6
tahirtcz <tahir@techchefz.com>
2025-05-28 13:57:22 +0530

    image lazy

--- scripts/aem.js
+++ scripts/aem.js
@@ -466,7 +466,7 @@ function decorateIcon(span, prefix = '', alt = '') {
   img.dataset.iconName = iconName;
   img.src = `${window.hlx.codeBasePath}${prefix}/icons/${iconName}.svg`;
   img.alt = alt;
-  img.loading = 'eager';
+  img.loading = 'lazy';
   span.append(img);
 }
 
