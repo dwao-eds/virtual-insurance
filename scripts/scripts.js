853f436e7465046489e45b49711778c0ba4402fd
tahirtcz <tahir@techchefz.com>
2025-05-28 13:10:48 +0530

    image and css changes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -241,7 +241,7 @@ async function loadLazy(doc) {
   loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
   loadFonts();
 
-  sampleRUM('lazy');
+  sampleRUM('eager');
   sampleRUM.observe(main.querySelectorAll('div[data-block-name]'));
   sampleRUM.observe(main.querySelectorAll('picture > img'));
 }
