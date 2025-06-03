53b7189e9820c2a95ef9c7f0137e470cec3ba156
jaytech989 <jay.vyas@techchefz.com>
2025-06-03 14:54:26 +0530

    added eager loadinng of images

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,6 +1,9 @@
+import { createOptimizedPicture } from "../../scripts/aem.js";
+// import { moveInstrumentation } from "../../scripts/scripts.js";
+
+
 export default function decorate(block) {
-
-
+  let newImg="";
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
 
@@ -11,6 +14,16 @@ export default function decorate(block) {
       const btn = document.querySelectorAll('.button');
       btn.forEach((btn)=>btn.setAttribute("target","_blank"));
       if (pic) {
+        const img=col.querySelector("picture > img");
+        const optimizedPic = createOptimizedPicture(img.src, img.alt, true, [
+          { width: "750" },
+        ]);
+        // moveInstrumentation(img, optimizedPic.querySelector("img"));
+        img.closest("picture").replaceWith(optimizedPic);
+        newImg = optimizedPic
+          .querySelector("picture > img")
+          .getAttribute("src");
+        
         const picWrapper = pic.closest('div');
         if (picWrapper && picWrapper.children.length === 1) {
           // picture is only content in column
